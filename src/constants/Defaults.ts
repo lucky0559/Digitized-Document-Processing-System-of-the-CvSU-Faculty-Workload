43ac64d75bbf6b57843255819b4ee8ca789737d7
Lucky Angelo Rabosa <angelorabosa55@yahoo.com.ph>
2022-08-27 15:32:30 +0800

    added hosted api url

--- src/constants/Defaults.ts
+++ src/constants/Defaults.ts
@@ -2,7 +2,6 @@ export const Default = {
   MINIMUM_PASSWORD_LENGTH: 8,
   ACCESS_TOKEN_STORAGE_KEY: "api/access_token",
   REFRESH_TOKEN_STORAGE_KEY: "api/refresh_token",
-  API_URL: "http://localhost:3000"
-  // API_URL:
-  //   "https://digitized-document-processing-system-of-the-cvsu-3op86gp1x.vercel.app/"
+  // API_URL: "http://localhost:3000/",
+  API_URL: "https://ddps-cvsu.herokuapp.com/"
 };
