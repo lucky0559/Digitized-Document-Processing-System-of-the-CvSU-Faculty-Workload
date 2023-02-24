a882f0b188dd99eea742c534424e3c421187ee03
Lucky Angelo <lrabosa@isbx.com>
2023-02-24 15:03:23 +0800

    updated teaching workload ui

--- src/screens/FacultyWorkload/TeachingWorkload/TeachingWorkLoad.tsx
+++ src/screens/FacultyWorkload/TeachingWorkload/TeachingWorkLoad.tsx
@@ -29,6 +29,8 @@ const TeachingWorkLoad = () => {
   const [totalNoOfStudents, setTotalNoOfStudents] = useState("");
   const [twlFile, setTwlFile] = useState<File>();
 
+  const [points, setPoints] = useState(0);
+
   const onSubmit = async () => {
     setTeachingWorkLoad({
       numberOfPreparations,
@@ -96,58 +98,69 @@ const TeachingWorkLoad = () => {
       <BodyContainer>
         <ScreenTitle title="Faculty Workload" />
         <Container>
-          <WorkloadTextContainer>
-            <WorkloadText>{WorkloadType.TEACHING_WORKLOAD}</WorkloadText>
-          </WorkloadTextContainer>
-          <InputsContainer>
-            <TextInputContainer>
-              <Label>Number of preparations</Label>
-              <TextInput
-                type="number"
-                value={numberOfPreparations}
-                onChange={e => numberOfPreparationsHandler(e.target.value)}
-              />
-            </TextInputContainer>
-            <TextInputContainer>
-              <Label>Contact Hours</Label>
-              <TextInput
-                type="number"
-                value={contactHours}
-                onChange={e => contactHoursHandler(e.target.value)}
-              />
-            </TextInputContainer>
-            <TextInputContainer>
-              <Label>Total No. of Students</Label>
-              <TextInput
-                type="number"
-                value={totalNoOfStudents}
-                onChange={e => totalNoOfStudentsHandler(e.target.value)}
-              />
-            </TextInputContainer>
-            <UploadFileContainer>
-              <Label>Upload class schedule here:</Label>
-              <UploadFileButtonContainer>
-                <UploadFileButton
-                  fileHandler={fileHandler}
-                  workloadFileName={twlFile?.name ? twlFile?.name : ""}
+          <SubContainer>
+            <WorkloadTextContainer>
+              <WorkloadText>{WorkloadType.TEACHING_WORKLOAD}</WorkloadText>
+            </WorkloadTextContainer>
+            <InputsContainer>
+              <Label style={{ marginBottom: 40, alignSelf: "flex-start" }}>
+                TWL = NP + CH + TNS(0.23)
+              </Label>
+              <TextInputContainer>
+                <Label style={{ fontWeight: "bold" }}>NP</Label>
+                <Label>Number of preparations</Label>
+                <TextInput
+                  type="number"
+                  value={numberOfPreparations}
+                  onChange={e => numberOfPreparationsHandler(e.target.value)}
                 />
-              </UploadFileButtonContainer>
-            </UploadFileContainer>
-          </InputsContainer>
-          <ButtonContainer>
-            <FormButton
-              text="Submit"
-              onClicked={onSubmit}
-              isSubmitting={isSubmitting}
-              disabled={
-                numberOfPreparations.length <= 0 ||
-                contactHours.length <= 0 ||
-                totalNoOfStudents.length <= 0 ||
-                twlFile?.name.length! <= 0 ||
-                twlFile?.name === undefined
-              }
-            ></FormButton>
-          </ButtonContainer>
+              </TextInputContainer>
+              <TextInputContainer>
+                <Label style={{ fontWeight: "bold" }}>CH</Label>
+                <Label>Contact Hours</Label>
+                <TextInput
+                  type="number"
+                  value={contactHours}
+                  onChange={e => contactHoursHandler(e.target.value)}
+                />
+              </TextInputContainer>
+              <TextInputContainer>
+                <Label style={{ fontWeight: "bold" }}>TNS</Label>
+                <Label>Total No. of Students</Label>
+                <TextInput
+                  type="number"
+                  value={totalNoOfStudents}
+                  onChange={e => totalNoOfStudentsHandler(e.target.value)}
+                />
+              </TextInputContainer>
+              <UploadFileContainer>
+                <Label>Upload class schedule here:</Label>
+                <UploadFileButtonContainer>
+                  <UploadFileButton
+                    fileHandler={fileHandler}
+                    workloadFileName={twlFile?.name ? twlFile?.name : ""}
+                  />
+                </UploadFileButtonContainer>
+              </UploadFileContainer>
+            </InputsContainer>
+            <ButtonContainer>
+              <Label style={{ fontWeight: "bold" }}>
+                Total Teaching Workload = {points.toString()}
+              </Label>
+              <FormButton
+                text="Submit"
+                onClicked={onSubmit}
+                isSubmitting={isSubmitting}
+                disabled={
+                  numberOfPreparations.length <= 0 ||
+                  contactHours.length <= 0 ||
+                  totalNoOfStudents.length <= 0 ||
+                  twlFile?.name.length! <= 0 ||
+                  twlFile?.name === undefined
+                }
+              ></FormButton>
+            </ButtonContainer>
+          </SubContainer>
         </Container>
       </BodyContainer>
     </MainContainer>
@@ -165,15 +178,25 @@ const BodyContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  border: 5px solid black;
+  border-radius: 15px;
+  margin: 120px auto;
 `;
 
 const Container = styled.div`
   padding: 30px;
-  width: 50%;
   align-items: center;
   justify-content: center;
   display: flex;
   flex-direction: column;
+`;
+
+const SubContainer = styled.div`
+  border: 2px solid black;
+  width: 100%;
+  height: auto;
+  border-radius: 15px;
+  padding: 15px;
 `;
 
 const WorkloadTextContainer = styled.div`
@@ -226,7 +249,8 @@ const UploadFileContainer = styled.div`
 
 const ButtonContainer = styled.div`
   display: flex;
-  align-self: flex-end;
+  justify-content: space-between;
+  align-items: center;
   margin: 20px 20px 0px 0px;
 `;
 
